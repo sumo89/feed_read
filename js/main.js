7dbb9a7638b4c52a44570191617692e00f04b345
Henry Summers <ssumo89@gmail.com>
2016-08-14 23:38:14 +0100

    added Guardian news

--- js/main.js
+++ js/main.js
@@ -7,7 +7,6 @@
 //http://content.guardianapis.com/search?from-date=2016-07-01&api-key=
 
 var app = {}
-
 //Nasa
 var nasaUrl = "https://api.nasa.gov/planetary/apod?api_key=qFJ1pZjIgYVKQO2VyfFgQLVfTd3OmyW8UB8OB9aQ";
 // var nasaUrlRandom = "https://api.nasa.gov/planetary/apod?date=" + dateYear + dateMonth + dateDay + "&api_key=qFJ1pZjIgYVKQO2VyfFgQLVfTd3OmyW8UB8OB9aQ";
@@ -46,7 +45,6 @@ app.generateRandom = function(e){
 		}
 	});
 };
-
 $.ajax({
 	url: nasaUrl,
 	success: function(result){
@@ -69,7 +67,6 @@ app.descriptionShow = function(e){
 	$("#nasaImageDesc").toggleClass('is-hidden');
 	$("#nasaDesctiption").toggleClass('is-hidden');
 };
-
 $('.nasaDescWrap').on('click', app.descriptionShow);
 $(".randomButton").on('click', app.generateRandom);
 
@@ -84,19 +81,16 @@ $.ajax({
 		$('#NatGeoCredit').html(result.credit);
 	}
 });
-//Guardian
-
 //Reddit
 app.redditAdd = function(e){
 	var searchWord = $('#redditSearch').val();
 		$.ajax({
 			url: "https://www.reddit.com/r/pics/search.json?q=" + searchWord + "&sort=new&restrict_sr=on&t=all",
 			success: function(result){
-				for (var i = 0; i < 11; i++){
+				for (var i = 0; i < 5; i++){
 					var searchResultImage = result.data.children[i].data.preview.images[0].source.url;
 					$('#redditImageWrap').addClass('hasImages')
 					$('#redditImageWrap').append('<img class="redditImage" src=' + searchResultImage + '>');
-					console.log("result 3");
 				};
 			}
 		});
@@ -105,25 +99,48 @@ app.redditSearch = function(e){
 	e.preventDefault();
 	if($('#redditImageWrap').hasClass('hasImages')){
 		$('.redditImage').remove();
-		console.log("removed");
 		app.redditAdd();
 	} else {
 		app.redditAdd();
-		console.log("added");
 	};
 };
 app.redditClear = function(e){
 	e.preventDefault();
 	$('.redditImage').remove();
-	console.log("removed");
 };
-
 $('#redditSubmit').on('click', app.redditSearch);
-
-
-
 
 // "https://www.reddit.com/r/pics/search.json?q=kittens&sort=new"
 
+//Guardian
+// 4aae3122-5e4e-414b-8341-7a81419b19e5
+//http://content.guardianapis.com/search?from-date=2016-07-01&api-key=
+// top newest news articles
+// webTitle - title
+// sectionName - type of news
+// webUrl - link
+
+var getDate = new Date();
+var month = getDate.getMonth()+1;
+var day = getDate.getDate();
+
+var dateOutput = getDate.getFullYear() + '-' +
+    (month<10 ? '0' : '') + month + '-' +
+    (day<10 ? '0' : '') + day;
+var guardianUrl = "http://content.guardianapis.com/search?from-date=" + dateOutput + "&api-key=4aae3122-5e4e-414b-8341-7a81419b19e5";
+$.ajax({
+	url: guardianUrl,
+	success: function(result){
+		var resultsRe = result.response.results;
+
+		$.each(resultsRe, function(key, value){
+			resultsRe.val = value;
+			var linkButton = '<div class="guardianLink"><a href="' + resultsRe.val.webUrl + '"</a> View on The Guardian</div>';
+			console.log("link-> " + linkButton);
+			$('#guardianWrap').append("<div class='guardianTitle'>" + resultsRe.val.webTitle + "</div>" + "<div class='guardianSection'>" + resultsRe.val.sectionName + "</div>" + linkButton);
+		});
+	}
+// response - results (array) - webTitle - sectionName - webUrl
+});
 
 
